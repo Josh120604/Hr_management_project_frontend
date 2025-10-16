eade13780f83012a13b1778f135ab709e86d4345
Josh120604 <joshuabihute@gmail.com>
2025-10-16 09:42:55 +0200

    changes
    Added demo login details for user reference.

--- src/components/LoginComp.vue
+++ src/components/LoginComp.vue
@@ -3,6 +3,8 @@
         <div class="signIn">
             <div class="signIn-header">
                 <h3>ModernTech Solutions</h3> <br> <hr> <br>
+                 <p> Demo Login details</p><br>
+                  <p> USERNAME : joshua     Password : Password123</p>
                 <div class="toggle-buttons">
                     <button class="h-50 w-25" @click="isSignIn = true" :class="{ 'active-toggle': isSignIn}">Sign In</button>
                     <button class="h-50 w-25" @click="isSignIn = false" :class="{ 'active-toggle': !isSignIn}">Register</button>
